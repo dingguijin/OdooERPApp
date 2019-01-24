c8594e51fc1d1d3d17ca29c4b482e47377c308e2
SuXueFeng <5392510@qq.com>
2019-01-24 11:53:43 +0800

    整理结构代码

---
+++ definition_theme/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "自定义主题界面风格",
+    'summary': """自定义界面风格模块""",
+    'description': """用以自定义Odoo界面风格的模块""",
+    'author': "SuXueFeng",
+    'website': "https://www.sxfblog.com",
+    'category': 'style',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'views/WebAssetsBackend.xml',
+    ],
+
+    'demo': [
+    ],
+}
